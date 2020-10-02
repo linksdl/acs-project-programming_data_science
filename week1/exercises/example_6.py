a1d5b720546ec76477e2af30021f0a5910180e91
linksdl <794469928@qq.com>
2020-10-02 09:58:24 +0100

    commit second

---
+++ week1/exercises/example_6.py
@@ -0,0 +1,15 @@
+x = 3
+
+type = type(x)
+
+print(type)
+
+print('this is a string')
+
+print('this is a quoted word: "hello" ')
+
+long_string = """this is a long string"""
+
+print(long_string)
+
+print("this is a string " * 3)
